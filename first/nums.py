89e718b17e63f46da575a04ebe281f50ebb7cc64
Alesia <alesik.sur@gmail.com>
2022-04-21 10:43:43 +0300

    add func multi and delete text add func dif

--- first/nums.py
+++ first/nums.py
@@ -1,10 +1,14 @@
 def plus(a, b):
     return a + b
 
-text = "Hallo"
+def multi(a, b):
+    return a * b
+
+
 
 def minus(a, b):
     return a - b
 
 print(plus(2, 2))
 print(minus(3, 2))
+print(multi(3, 4))
---
+++ first/strings.py
@@ -0,0 +1,4 @@
+def dif(a, b):
+    return a / b
+
+print(dif(12, 4))
